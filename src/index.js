4ec64f1eaf81a314a95281df29cc3407bd5a25d6
Josué Caycho Bardalez <jcaycho@sapia.com.pe>
2022-04-03 20:39:34 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+//Servidor
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+//Activar uso de variables de entorno
+require("dotenv").config();
+
+//Path de archivo publico
+const path = require('path')
+
+//Midleware
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+//Controladores
+// app.use('ruta de controladores')
+
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+    console.log('A user connected');
+ 
+    //Whenever someone disconnects this piece of code executed
+    socket.on('disconnect', function () {
+       console.log('A user disconnected');
+    });
+ });
+
+const PORT = process.env.PORT || 3000
+console.log('PROCCESS', process.env.PORT)
+
+server.listen(PORT, function() {
+    console.log('Escuchando en el puerto *:', PORT);
+ });
